5eb56ca9cb1de857aa88488ca57d8eb002c111f5
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-03-07 16:16:16 +0300

    added color matching game route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { lazy, Suspense } from "react";
 /** lazy loading the game components */
 const Home = lazy(() => import("./views/home/Home"));
 const NotFound = lazy(() => import("./pages/NotFound"));
+const ColorMatching = lazy(() =>
+  import("./views/color-matching/ColorMatching")
+);
 
 const Loading = () => {
   return <p>loading...</p>;
@@ -15,6 +18,7 @@ function App() {
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route exact path="/" element={<Home />} />;
+          <Route path="/color-matching" element={<ColorMatching />} />
           <Route path={"*"} element={<NotFound />} />;
         </Routes>
       </Suspense>
