ecd619795ce4f031904f3822c4872e24921b45e3
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-04-01 21:14:02 +0300

    added match country flags to home page

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
     { to: "/guess-capital-city", name: "guess-capital" },
     { to: "/guess-animal", name: "guess-animal" },
     { to: "/match-president", name: "match-president" },
+    { to: "/match-country-flags", name: "match-country-flag" },
   ];
   return (
     <Box textAlign="center" fontSize="xl">
